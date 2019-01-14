caf71dda10de1e9bed62eef20dff9f9feb0badff
Aidan <aidan@gene.co.uk>
2019-01-14 09:43:32 +0000

    Module shell, paypal plugin override and system.xml port

---
+++ Plugin/PayPal/CartButton.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Gene\BraintreeHiConversion\Plugin\PayPal;
+
+/**
+ * Class CartButton
+ * @package Gene\BraintreeHiConversion\Plugin\PayPal
+ */
+class CartButton
+{
+    const TEMPLATE_PATH = 'Gene_BraintreeHiConversion::paypal/cartbutton.phtml';
+
+    public function __construct(
+    ) {
+    }
+
+    /**
+     * Modify the template for the PayPal button block only if the config options to "test" are enabled.
+     * @param \Magento\Braintree\Block\Paypal\Button $subject
+     * @param $result
+     * @return string
+     */
+    public function afterGetTemplate(\Magento\Braintree\Block\Paypal\Button $subject, $result)
+    {
+        return self::TEMPLATE_PATH;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# HiConversion Braintree Integration
+
+1. PayPal Button Override in mini+cart - checkout for styling etc
