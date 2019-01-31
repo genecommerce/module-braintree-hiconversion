84b3c445e0f244f1f45c7b70ea79760645d8c071
Aidan <aidan@gene.co.uk>
2019-01-31 11:43:22 +0000

    Product Page Button

---
+++ Plugin/PayPal/ProductButton.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Gene\BraintreeHiConversion\Plugin\PayPal;
+
+/**
+ * Class CartButton
+ * Replace the template used by the product button to use the HIC one
+ * @package Gene\BraintreeHiConversion\Plugin\PayPal
+ */
+class CartButton
+{
+    const TEMPLATE_PATH = 'Gene_BraintreeHiConversion::paypal/productbutton.phtml';
+
+    /**
+     * @var \Hic\Integration\Helper
+     */
+    protected $helper;
+
+    /**
+     * CartButton constructor.
+     * @param \Gene\BraintreeHiConversion\Helper\Data $helper
+     */
+    public function __construct(
+        \Gene\BraintreeHiConversion\Helper\Data $helper
+    ) {
+        $this->helper = $helper;
+    }
+
+    /**
+     * Modify the template for the PayPal button block only if the config options to "test" are enabled.
+     * @param \Magento\Braintree\Block\Paypal\Button $subject
+     * @param $result
+     * @return string
+     */
+    public function afterGetTemplate(\Magento\Braintree\Block\Paypal\Button $subject, $result)
+    {
+        // @todo change setting to check against product
+        if ($this->helper->isTestingEnabled()) {
+            return self::TEMPLATE_PATH;
+        }
+
+        return $result;
+    }
+}
