1413933a0542262f287c2acafa6513874e49a4b8
Aidan <aidan@gene.co.uk>
2019-01-31 10:42:26 +0000

    expose label, branding and fundingoptions for paypal btn

--- view/frontend/web/js/paypal/button.js
+++ view/frontend/web/js/paypal/button.js
@@ -18,22 +18,23 @@ define([
                     shape: this.config.shape,
                     size: this.config.size,
                     layout: this.config.layout,
+                    label: this.config.label,
+                    branding: this.config.branding,
+                    fundingicons: this.config.fundingicons,
                     disabledFunding: this.config.disabledFunding
                 };
 
-                // @todo call HIC's API with a promise?
-                // Pass through _config which HIC can modify and return
-                // the callback here then calls new Button(config);
-
                 var minicartWrapper = $('#' + this.config.id).closest('#minicart-content-wrapper');
-
-
+                
                 hicCore.interceptPaypalButton(minicartWrapper.length ? 'minicart': 'cart', _config, function (_config) {
                     this.config.color = _config.color;
                     this.config.shape = _config.shape;
                     this.config.size = _config.size;
                     this.config.layout = _config.layout;
                     this.config.disabledFunding = _config.disabledFunding;
+                    this.config.label = _config.label;
+                    this.config.branding = _config.branding;
+                    this.config.fundingicons = _config.fundingicons;
                     new Button(this.config);
                 }.bind(this));
             
