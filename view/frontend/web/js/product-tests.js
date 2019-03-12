163986554ae5ee9825b5570ce5157bee4095e988
Mark Conatser <mconatser@hiconversion.com>
2019-03-12 14:43:27 -0400

    Add active on cart check to pdp method add

--- view/frontend/web/js/product-tests.js
+++ view/frontend/web/js/product-tests.js
@@ -9,7 +9,8 @@ define([
     'use strict';
 
     return function (config) {
-        if (config && config.isPaypalActive && config.isPaypalActiveOnPdp) {
+        // active on cart check is only required here as long as the main module is requiring that before injecting pdp button
+        if (config && config.isPaypalActive && config.isPaypalActiveOnCart && config.isPaypalActiveOnPdp) {
             hicCore.paymentMethods().add({
                 configTest: config,
                 page: 'pdp',
