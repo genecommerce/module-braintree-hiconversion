42d49be3f3a65b84b1fbbc301fc257621d6ff772
Adi <agano90@gmail.com>
2019-08-01 22:42:01 -0400

    Sync show with render; add render only when diff

--- view/frontend/web/js/payment-method.js
+++ view/frontend/web/js/payment-method.js
@@ -10,14 +10,16 @@ define([
 
     return {
 
-        new: function(args){
+        new: function (args) {
             var obj = {
                 page: args.page,
                 type: args.type,
                 device: args.device,
                 needs: args.needs,
+                canTestWallet: canTestWallet,
                 config: {},
                 defaultConfig: {},
+                sameConfig: sameConfig,
                 missing: [],
                 getEnabled: getEnabled,
                 setEnabled: setEnabled,
@@ -40,28 +42,28 @@ define([
                 extendDesiredConfig: extendDesiredConfig,
                 applyDesiredConfig: applyDesiredConfig,
                 extendAndApplyDesiredConfig: extendAndApplyDesiredConfig,
-            }            
-            function postMessage(args){
+            }
+            function postMessage(args) {
                 var loc = document.location || window.location;
                 var origin = loc.origin || loc.protocol + "//" + loc.host;
                 var baseConfig = {
                     group: 'update-bt-ge-hi',
-                    cat: 'paymentMethod',                    
+                    cat: 'paymentMethod',
                     page: obj.page,
                     type: obj.type,
                     device: obj.device,
                 };
                 var config = $.extend({}, baseConfig, args);
-                window.postMessage({config}, origin);
+                window.postMessage({ config }, origin);
             }
             function getUniqId() {
                 return Date.now() + Math.random().toString().slice(2);
             };
-            function deregister(id){
+            function deregister(id) {
                 var degistered = false;
-                $.each(obj.listeners, function(eventType,eventTypeListeners){
-                    $.each(eventTypeListeners, function(eventId,cb){                        
-                        if (eventId === id){
+                $.each(obj.listeners, function (eventType, eventTypeListeners) {
+                    $.each(eventTypeListeners, function (eventId, cb) {
+                        if (eventId === id) {
                             degistered = true;
                             delete obj.listeners[eventType][eventId];
                         }
@@ -69,57 +71,76 @@ define([
                 });
                 return degistered
             }
-            function register(type, cb){
+            function register(type, cb) {
                 /* Events: Click, Cancel, Error, and Render */
                 var eventId = getUniqId();
-                if (typeof(cb) === 'function'){
+                if (typeof (cb) === 'function') {
                     obj.listeners[type][eventId] = cb;
                     return eventId;
-                }else{
+                } else {
                     return "must be a function";
                 }
             }
-            function onEvent(store, callback){
-                $.each(store, function(eventId, cb){
+            function onEvent(store, callback) {
+                $.each(store, function (eventId, cb) {
                     cb(callback)
                 });
                 return obj;
             }
-            function onClick(callback){
+            function onClick(callback) {
                 return onEvent(obj.listeners.click, callback);
             }
-            function onCancel(callback){
+            function onCancel(callback) {
                 return onEvent(obj.listeners.cancel, callback);
             }
-            function onError(callback){
-                return onEvent(obj.listeners.error, callback);    
+            function onError(callback) {
+                return onEvent(obj.listeners.error, callback);
             }
-            function onRender(callback){
+            function onRender(callback) {
                 return onEvent(obj.listeners.render, callback);
             }
-            function styleElem(){
-                var elem = $('style[data-page="'+obj.page+'"][data-type="'+obj.type+'"]');
+            function styleElem() {
+                var elem = $('style[data-page="' + obj.page + '"][data-type="' + obj.type + '"]');
                 return (elem.length === 0) ? false : elem;
             }
-            function show(force){                
-                if (styleElem() !== false){
-                    if (obj.testing !== false || force === true){
-                        styleElem().remove();
+            function show(force) {
+                var cssShow = function(){
+                    postMessage({
+                        m: 'show',
+                        testing: obj.testing,
+                        force: force
+                    })
+                    if (styleElem() !== false) {
+                        if (obj.testing !== false || force === true) {
+                            styleElem().remove();
+                        }
                     }
                 }
-                postMessage({
-                    m: 'show',
-                    testing: obj.testing,
-                    force: force
-                })
+                var renderId;
+                var paypalButtonRendered = function(){
+                    deregister(renderId);
+                    cssShow();
+                }
+                if (obj.type === 'applePay' || obj.type === 'googlePay') {
+                    cssShow();
+                } else if (obj.type === 'paypalCredit' || obj.type === 'paypal') {
+                    if (sameConfig() === true && buttonFound() !== 0){
+                        cssShow();
+                    } else {
+                        update(desiredConfig().config);
+                        addButton();
+                        renderId = register('render', paypalButtonRendered);
+                    }
+                }
+
                 return obj;
             }
-            function hide(){
-                if (styleElem() === false){
+            function hide() {
+                if (styleElem() === false) {
                     var text = obj.elem.selector + '{display:none;}';
                     var styleSheet = document.createElement('style');
-                        styleSheet.setAttribute('data-page', obj.page);
-                        styleSheet.setAttribute('data-type', obj.type);
+                    styleSheet.setAttribute('data-page', obj.page);
+                    styleSheet.setAttribute('data-type', obj.type);
                     document.getElementsByTagName('head')[0].appendChild(styleSheet);
                     if (styleSheet.styleSheet) {
                         styleSheet.styleSheet.cssText = text;
@@ -132,26 +153,33 @@ define([
                 })
                 return obj;
             }
-            function update(newConfig){
+            function setShowState(showState){
+                if (showState){
+                    show(true);
+                }else{
+                    hide();
+                }
+            }
+            function update(newConfig) {
                 $.extend(true, obj.config, newConfig);
                 postMessage({
-                    m: 'paymentMethod.update',                    
+                    m: 'paymentMethod.update',
                 })
                 return obj
             }
-            function addButton(arg){
-                if (arg !== false){
+            function addButton(arg) {
+                if (arg !== false) {
                     removeButton();
                 }
-                if (obj.paypalHook !== undefined && typeof(obj.paypalHook) === 'function'){
+                if (obj.paypalHook !== undefined && typeof (obj.paypalHook) === 'function') {
                     obj.paypalHook(obj.config);
                 }
                 postMessage({
-                    m: 'paymentMethod.addButton',                                                        
+                    m: 'paymentMethod.addButton',
                 });
                 return obj;
             }
-            function addPaypal(config, cb){
+            function addPaypal(config, cb) {
                 obj.defaultConfig = config;
                 obj.config = $.extend(true, {}, obj.defaultConfig, obj.config);
                 obj.config.events = (obj.config.events === undefined) ? {} : obj.config.events;
@@ -166,98 +194,125 @@ define([
                 })
                 return obj;
             }
-            function desiredConfig(){
+            function desiredConfig() {
                 return window.braintreeHicApi.desiredConfig[args.page][args.type]
             }
-            function extendDesiredConfig(desiredConfig){
-               var oldDesiredConfig = window.braintreeHicApi.desiredConfig[obj.page][obj.type];               
-               var tempConfig = $.extend(true, {}, obj.defaultConfig, oldDesiredConfig.config);
-               var newDesiredConfig = {
-                   show: (desiredConfig.show === undefined) ? oldDesiredConfig.show : desiredConfig.show,
-                   config: (desiredConfig.config === undefined) ? tempConfig : $.extend(true, {}, tempConfig, desiredConfig.config)
-               }
-               return window.braintreeHicApi.desiredConfig[obj.page][obj.type] = newDesiredConfig;
-            }
-            function applyDesiredConfig(){
-                var renderEventId;
-                function paypalButtonLoadedShow(){
-                    deregister(renderEventId);
-                    show(true);
+            function extendDesiredConfig(desiredConfig) {
+                var oldDesiredConfig = window.braintreeHicApi.desiredConfig[obj.page][obj.type];
+                var tempConfig = $.extend(true, {}, obj.defaultConfig, oldDesiredConfig.config);
+                var newDesiredConfig = {
+                    show: (desiredConfig.show === undefined) ? oldDesiredConfig.show : desiredConfig.show,
+                    config: (desiredConfig.config === undefined) ? tempConfig : $.extend(true, {}, tempConfig, desiredConfig.config)
                 }
-                var desiredConfig = obj.desiredConfig();
-                if (obj.type === 'paypalCredit' || obj.type === 'paypal'){
-                    if (obj.test && obj.test.isTestingEnabled && getEnabled() && window.braintreeHicApi.timing.hicLate === false){
-                        if (desiredConfig.show){
-                            renderEventId = obj.register('render', paypalButtonLoadedShow);
-                        }else{
-                            hide();
-                        }
-                        update(desiredConfig.config);
-                        addButton();
-                    }else{
-                        addButton();
-                        show(true);
-                    }
-                }else if (obj.type === 'applePay' || obj.type === 'googlePay'){
-                    if (obj.test && obj.test.isTestingEnabled && getEnabled() && window.braintreeHicApi.timing.hicLate === false){
-                        if (desiredConfig.show){
-                            show(true);
-                        }else{
-                            hide();
-                        }
-                    }else{
-                        show(true);
-                    }
+                return window.braintreeHicApi.desiredConfig[obj.page][obj.type] = newDesiredConfig;
+            }
+            function canTestWallet(){
+                if (obj.test && obj.test.isTestingEnabled && getEnabled() && window.braintreeHicApi.timing.hicLate === false){
+                    return true
+                }else{
+                    return false;
                 }
             }
-            function extendAndApplyDesiredConfig(desiredConfig){
+            function sameConfig(){
+                var curConfig = $.extend(true, {}, obj.config);
+                var desiredConfig = $.extend(true, {}, obj.desiredConfig().config);
+                var dontCompare = ['events'];
+                $.each(dontCompare, function(i,prop){
+                    if (curConfig.hasOwnProperty(prop)){
+                        delete curConfig[prop];
+                    }
+                })
+                function isEqual(value, other) {
+                    var type = Object.prototype.toString.call(value);
+                    if (type !== Object.prototype.toString.call(other)) return false;
+                    if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
+                    var valueLen = type === '[object Array]' ? value.length : Object.keys(value).length;
+                    var otherLen = type === '[object Array]' ? other.length : Object.keys(other).length;
+                    if (valueLen !== otherLen) return false;
+                    var compare = function (item1, item2) {
+                        var itemType = Object.prototype.toString.call(item1);
+                        if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+                            if (!isEqual(item1, item2)) return false;
+                        }
+                        else {
+                            if (itemType !== Object.prototype.toString.call(item2)) return false;
+                            if (itemType === '[object Function]') {
+                                if (item1.toString() !== item2.toString()) return false;
+                            } else {
+                                if (item1 !== item2) return false;
+                            }
+                        }
+                    };
+                    if (type === '[object Array]') {
+                        for (var i = 0; i < valueLen; i++) {
+                            if (compare(value[i], other[i]) === false) return false;
+                        }
+                    } else {
+                        for (var key in value) {
+                            if (value.hasOwnProperty(key)) {
+                                if (compare(value[key], other[key]) === false) return false;
+                            }
+                        }
+                    }
+                    return true;
+                };
+                return isEqual(curConfig, desiredConfig);
+            }            
+            function applyDesiredConfig() {
+                if(canTestWallet()){
+                    setShowState(desiredConfig().show);
+                }else{
+                    setShowState(true);
+                }
+            }
+            function extendAndApplyDesiredConfig(desiredConfig) {
                 var newConfig = obj.extendDesiredConfig(desiredConfig);
                 obj.applyDesiredConfig(newConfig);
             }
-            function removeButton(){
+            function removeButton() {
                 $(obj.elem.buttonSelector).remove();
                 postMessage({
                     m: 'paymentMethod.removeButton',
                 })
                 return obj;
             }
-            function found(){
+            function found() {
                 return $(obj.elem.selector).length;
             }
-            function buttonFound(){
+            function buttonFound() {
                 return $(obj.elem.buttonSelector).length
             }
-            function eligible(){
+            function eligible() {
                 var e = {
                     config: true,
                     test: true,
                 }
-                if (args.type === 'applePay'){
+                if (args.type === 'applePay') {
                     e.secure = (location.protocol === 'https:') ? true : false;
                     e.apple_device = false;
                     e.proper_device = false;
-                    e.user = false;  
-                    if (window.ApplePaySession !== undefined){
+                    e.user = false;
+                    if (window.ApplePaySession !== undefined) {
                         e.apple_device = true;
-                        if (window.ApplePaySession.canMakePayments() === true){
+                        if (window.ApplePaySession.canMakePayments() === true) {
                             e.proper_device = true;
                             var merchant_id = '';
                             ApplePaySession.canMakePaymentsWithActiveCard(merchant_id).then(function (canMakePayments) {
-                                e.user = canMakePayments;                
+                                e.user = canMakePayments;
                             })
                         }
-                    }             
+                    }
                 }
                 return e;
-            }    
-            function findStatus(){
+            }
+            function findStatus() {
                 return {
                     visible: (styleElem() !== false && buttonFound() !== 0) ? false : true,
                     found: (found() === 1) ? true : false,
                     enableShow: (obj.testing === false) ? false : true,
                 };
             }
-            function add_paypal_methods(){
+            function add_paypal_methods() {
                 $.extend(true, obj, {
                     addPaypal: addPaypal,
                     elem: {
@@ -279,21 +334,21 @@ define([
                     onError: onError,
                     onRender: onRender
                 });
-            }            
-            function setEnabled(args, clear){
+            }
+            function setEnabled(args, clear) {
                 return obj.needs = (clear === true) ? args : $.extend(true, obj.needs, args);
             }
-            function getEnabled(){
+            function getEnabled() {
                 obj.missing = [];
-                $.each(obj.needs, function(i,need){
-                    if (obj.test && obj.test.config && obj.test.config[need] !== true){
+                $.each(obj.needs, function (i, need) {
+                    if (obj.test && obj.test.config && obj.test.config[need] !== true) {
                         obj.missing.push(need);
                     }
                 })
                 return (obj.missing.length === 0) ? true : false;
             }
-            function init(){
-                if (obj.type === 'paypal' || obj.type === 'paypalCredit'){
+            function init() {
+                if (obj.type === 'paypal' || obj.type === 'paypalCredit') {
                     add_paypal_methods();
                 }
 
@@ -303,11 +358,11 @@ define([
                 obj.test.config = konfig;
 
                 obj.extendAndApplyDesiredConfig({});
-  
+
                 return obj;
             }
             return obj;
         },
-              
+
     }
 });
